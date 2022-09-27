1cbd9ac9caf294d7511fe636d6e352bd6f238b69
sonalinf <56978784+sonalinf@users.noreply.github.com>
2022-09-27 14:22:34 -0700

    format change

--- Client_DTMC.py
+++ Client_DTMC.py
@@ -5,18 +5,18 @@ import sys
 HOST = 'localhost'
 BUFSIZE = 1024
 
-def ipv6_echo_client(port, host=HOST): 
+def ipv6_echo_client(port, host=HOST):
     for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
         af, socktype, proto, canonname, sa = res
-         try:
-         sock = socket.socket(af, socktype, proto)
-         except socket.error as err:
-         print ("Error:%s" %err)
-    try:
-        sock.connect(sa)
-    except socket.error as msg:
-        sock.close()
-        continue
+        try:
+            sock = socket.socket(af, socktype, proto)
+        except socket.error as err:
+            print ("Error:%s" %err)
+        try:
+            sock.connect(sa)
+        except socket.error as msg:
+            sock.close()
+            continue
     if sock is None:
         print ('Failed to open socket!')
         sys.exit(1)
@@ -26,17 +26,14 @@ def ipv6_echo_client(port, host=HOST):
     while True:
         data = sock.recv(BUFSIZE)
         print ('Received from server', repr(data))
-        if not data:
+        if not data: 
             break
-        sock.close()
-        
-if __name__ == '__main__':
+    sock.close()
+    
+
+if __name__ == '__main__': 
     parser = argparse.ArgumentParser(description='IPv6 socket client example')
     parser.add_argument('--port', action="store", dest="port", type=int, required=True)
-    given_args = parser.parse_args()
+    given_args = parser.parse_args() 
     port = given_args.port
     ipv6_echo_client(port)
-
-
-
-         
--- Server_CDTA.py
+++ Server_CDTA.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse 
 import socket
 import sys
 
@@ -6,12 +6,13 @@ HOST = 'localhost'
 
 def echo_server(port, host=HOST):
     """Echo server using IPv6 """
-    for result in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE): 
+    for result in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
         af, socktype, proto, canonname, sa = result
         try:
             sock = socket.socket(af, socktype, proto)
         except socket.error as err:
             print ("Error: %s" %err)
+        
         try:
             sock.bind(sa)
             sock.listen(1)
@@ -24,17 +25,16 @@ def echo_server(port, host=HOST):
     conn, addr = sock.accept()
     print ('Connected to', addr)
     while True:
-         data = conn.recv(1024)
-         print ("Received data from the client: [%s]" %data)
-         if not data: break
-         conn.send(data)
-         print ("Sent data echoed back to the client: [%s]" %data)
-        conn.close()
+        data = conn.recv(1024)
+        print ("Received data from the client: [%s]" %data)
+        if not data: break
+        conn.send(data)
+        print ("Sent data echoed back to the client: [%s]" %data)
+    conn.close()
+
 if __name__ == '__main__':
-     parser = argparse.ArgumentParser(description='IPv6 Socket
-     Server Example')
-     parser.add_argument('--port', action="store", dest="port",
-     type=int, required=True)
-     given_args = parser.parse_args()
-     port = given_args.port
-     echo_server(port)
+    parser = argparse.ArgumentParser(description='IPv6 Socket Server Example')
+    parser.add_argument('--port', action="store", dest="port", type=int, required=True)
+    given_args = parser.parse_args() 
+    port = given_args.port
+    echo_server(port)
