d5b1f88fdba202b575ea05f274216a28dad3bff6
sonalinf <56978784+sonalinf@users.noreply.github.com>
2022-10-01 10:46:48 -0700

    changed server code

--- DTMC-CDTA-server-client/Server/Server.py
+++ DTMC-CDTA-server-client/Server/Server.py
@@ -6,7 +6,8 @@ HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 #SERVER = "71.193.200.239"
-ADDR = (SERVER, PORT)
+#ADDR = (SERVER, PORT)
+ADDR = ('', PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
