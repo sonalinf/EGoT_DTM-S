39985d3cdd9fb920228f5f4584ce516062697094
sonalinf <56978784+sonalinf@users.noreply.github.com>
2022-10-01 12:25:38 -0700

    Update client.py

--- DTMC-CDTA-server-client/Testing/client.py
+++ DTMC-CDTA-server-client/Testing/client.py
@@ -4,8 +4,8 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-#SERVER = "192.168.0.2" 
-SERVER = "10.200.147.231"
+SERVER = "192.168.0.2" 
+#SERVER = "10.200.147.231"
 # replace with webaddres such as:
 #    remote_host = 'www.python.org'
 #get the ipaddress of this computer by name
