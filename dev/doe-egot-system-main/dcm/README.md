8fd0ff47b4805a8abdcc0ce1758a17515a423bce
sonalinf <56978784+sonalinf@users.noreply.github.com>
2022-11-21 01:07:45 -0800

    Delete dev/doe-egot-system-main directory

--- dev/doe-egot-system-main/dcm/README.md
+++
@@ -1,2 +0,0 @@
-# doe-egot-dcm
-Energy Grid of Things: Distributed Control Module
--- dev/doe-egot-system-main/ssl/cert_chain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-files="root-ca/client*.crt"
-for file in $files
-do 
-    openssl x509 -in $file >> root-ca/cert_chain.crt
-done
